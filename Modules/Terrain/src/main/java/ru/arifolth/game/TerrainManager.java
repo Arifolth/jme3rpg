7444a2ee4dd6f42a18ea50f207e1394f13b21a32
alexander <arifolth@gmail.com>
2021-04-13 03:27:06 +0300

    License improvements

--- Modules/Terrain/src/main/java/ru/arifolth/game/TerrainManager.java
+++ Modules/Terrain/src/main/java/ru/arifolth/game/TerrainManager.java
@@ -1,5 +1,6 @@
 /**
- *     Copyright (C) 2021  Alexander Nilov
+ *     ANJRpg - an open source Role Playing Game written in Java.
+ *     Copyright (C) 2021 Alexander Nilov
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
